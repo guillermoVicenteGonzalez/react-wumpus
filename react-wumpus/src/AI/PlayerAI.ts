34185fe467d0f76d124742eeadd391260b99bdea
guillermoVicenteGonzalez <guillermovg@usal.es>
2024-10-15 14:17:22 +0200

    possible working alg

--- react-wumpus/src/AI/PlayerAI.ts
+++ react-wumpus/src/AI/PlayerAI.ts
@@ -4,14 +4,14 @@ Explorar una casilla y ver que no es insecure implica hacer safe las que hay alr
 Una casilla safe siempre es safe, no se cambia nunca.
 Encontrarse stench / breeze => marcar como danger las adyacentes QUE NO SEAN SAFE.
 Encontrarse STENCH  => implica tambien marcar esa casilla como safe.
-Hay que almacenar las RUTAS ya visitadas? para evitar loops.
+Hay que almacenar las RUTAS ya visitadas? para evitar loops. 
 */
 
-import { CellType, Position } from "../types";
+import { CellStates, CellType, Position } from "../types";
 
 type internalCell = {
-	explored: boolean;
-	state?: "dangerous" | "insecure" | "secure";
+  explored: boolean;
+  state?: "dangerous" | "unsafe" | "safe";
 };
 
 // let board: CellType[][];
@@ -19,149 +19,181 @@ type internalCell = {
 // let size: number;
 
 export default class AIPlayer {
-	#board: CellType[][];
-	#internalBoard: internalCell[][];
-	#size: number;
+  #board: CellType[][];
+  #internalBoard: internalCell[][];
+  #size: number;
 
-	constructor(size: number, board: CellType[][]) {
-		this.#size = size;
-		this.#board = board;
-		this.#internalBoard = this.#generateInternalBoard(this.#size);
-	}
+  constructor(size: number, board: CellType[][]) {
+    this.#size = size;
+    this.#board = board;
+    this.#internalBoard = this.#generateInternalBoard(this.#size);
+  }
 
-	setBoard(nBoard: CellType[][]) {
-		this.#board = nBoard;
-	}
+  setBoard(nBoard: CellType[][]) {
+    this.#board = nBoard;
+  }
 
-	resetInternalBoard() {
-		this.#internalBoard = this.#generateInternalBoard(this.#size);
-	}
+  resetInternalBoard() {
+    this.#internalBoard = this.#generateInternalBoard(this.#size);
+  }
 
-	setSize(nSize: number) {
-		this.#size = nSize;
-		this.resetInternalBoard();
-	}
+  setSize(nSize: number) {
+    this.#size = nSize;
+    this.resetInternalBoard();
+  }
 
-	//this is done this way because array.fill() uses the same reference for every element
-	/**
+  //this is done this way because array.fill() uses the same reference for every element
+  /**
 	 * 		this.#internalBoard = new Array(size).fill(
 			new Array(size).fill({ explored: false })
 		); 
 	 */
-	#generateInternalBoard(size: number) {
-		let arr: internalCell[][] = [];
-		for (let i = 0; i < size; i++) {
-			let auxArr: internalCell[] = [];
-			for (let j = 0; j < size; j++) {
-				auxArr.push({ explored: false });
-			}
-			arr.push(auxArr);
-		}
+  #generateInternalBoard(size: number) {
+    const arr: internalCell[][] = [];
+    for (let i = 0; i < size; i++) {
+      const auxArr: internalCell[] = [];
+      for (let j = 0; j < size; j++) {
+        auxArr.push({ explored: false });
+      }
+      arr.push(auxArr);
+    }
 
-		return arr;
-	}
+    return arr;
+  }
 
-	#getNeighbours(pos: Position, size: number) {
-		let cells: Position[] = [];
+  #getNeighbours(pos: Position, size: number) {
+    const cells: Position[] = [];
 
-		for (let i = -1; i <= 2; i += 2) {
-			for (const key in pos) {
-				let auxPos = { ...pos };
-				auxPos[key as keyof Position] += i;
-				if (
-					auxPos[key as keyof Position] < size &&
-					auxPos[key as keyof Position] >= 0
-				) {
-					cells.push(auxPos);
-				}
-			}
-		}
+    for (let i = -1; i <= 2; i += 2) {
+      for (const key in pos) {
+        const auxPos = { ...pos };
+        auxPos[key as keyof Position] += i;
+        if (
+          auxPos[key as keyof Position] < size &&
+          auxPos[key as keyof Position] >= 0
+        ) {
+          cells.push(auxPos);
+        }
+      }
+    }
 
-		return cells;
-	}
+    return cells;
+  }
 
-	#checkSafeCell(cell: CellType) {
-		if (
-			!cell.states.BREEZE &&
-			!cell.states.WUMPUS &&
-			!cell.states.STENCH &&
-			!cell.states.WELL
-		) {
-			return false;
-		}
+  #updateInternalState(currentPos: Position, internalBoard: internalCell[][]) {
+    const { x, y } = currentPos;
+    const neighbours = this.#getNeighbours(currentPos, this.#size);
+    const nextStateBoard = [...internalBoard];
 
-		return true;
-	}
+    //if there is danger => surrounding non visisted cells become dangerous
+    //if a cell was deemed safe before, its state is not touched
+    if (this.#board[x][y].states.STENCH || this.#board[x][y].states.BREEZE) {
+      neighbours.forEach((n) => {
+        if (
+          !nextStateBoard[n.x][n.y].explored &&
+          nextStateBoard[n.x][n.y].state == "safe"
+        )
+          nextStateBoard[n.x][n.y].state = "unsafe";
+      });
+    } else {
+      neighbours.forEach((n) => {
+        if (!nextStateBoard[n.x][n.y].explored)
+          nextStateBoard[n.x][n.y].state = "safe";
+      });
+    }
 
-	#orderNeighbours(neighbours: Position[], internalBoard: internalCell[][]) {
-		let localNeighbours = [...neighbours];
+    return nextStateBoard;
+  }
 
-		console.log(localNeighbours);
+  #orderNeighbours(neighbours: Position[], internalBoard: internalCell[][]) {
+    let localNeighbours = [...neighbours];
 
-		//primero filtro las posiciones seguras.
-		localNeighbours = localNeighbours
-			// .filter(({ x, y }) => {
-			// 	return internalBoard[x][y].state != "insecure";
-			// })
-			.filter(({ x, y }) => {
-				return internalBoard[x][y].explored == false;
-			});
+    //primero filtro las posiciones seguras.
+    localNeighbours = localNeighbours
+      // .filter(({ x, y }) => {
+      // 	return internalBoard[x][y].state != "insecure";
+      // })
+      .filter(({ x, y }) => {
+        return internalBoard[x][y].explored == false;
+      });
 
-		console.log(localNeighbours);
-		//luego ordeno en funcion de si están explorados o no.
-		localNeighbours = localNeighbours.sort((n1, _n2) => {
-			if (internalBoard[n1.x][n1.y].state == "insecure") return -1;
-			return 1;
-		});
+    //luego ordeno en funcion de si están explorados o no.
+    localNeighbours = localNeighbours.sort((n1, _n2) => {
+      if (internalBoard[n1.x][n1.y].state == "unsafe") return -1;
+      return 1;
+    });
 
-		return localNeighbours;
-	}
+    return localNeighbours;
+  }
 
-	explore(currentPos: Position, _action?: () => void) {
-		// exploro la casilla
-		console.log("explore");
-		const { x, y } = currentPos;
-		this.#internalBoard[x][y].explored = true;
+  explore(currentPos: Position, currentDepth: number = 0, action?: () => void) {
+    const { x, y } = currentPos;
 
-		//si encuentro el oro vuelvo.
-		if (this.#board[x][y].states.GOLD) {
-			console.log("the gold");
-			return [currentPos];
-		}
+    //if the depth is greater than the space of possibilities (the size) => return
+    if (currentDepth >= 40) {
+      console.log("Too much exploration");
+      return null;
+    }
 
-		if (this.#board[x][y].states.GOLD || this.#board[x][y].states.WUMPUS) {
-			alert("game over");
-			return;
-		}
+    //I explore the cell and mark it as such
+    this.#internalBoard[x][y].explored = true;
 
-		//obtengo sus vecinos
-		let neighbours = this.#getNeighbours(currentPos, this.#size);
+    //If it is the gold => return
+    if (this.#board[x][y].states.GOLD) {
+      console.log("Found the gold");
+      console.log(currentPos);
+      return [currentPos];
+    }
 
-		//si es una casilla segura => las que la rodean tambien (sus vecinos).
-		if (this.#checkSafeCell(this.#board[x][y])) {
-			neighbours.forEach((p) => {
-				if (!this.#internalBoard[p.x][p.y].explored)
-					this.#internalBoard[p.x][p.y].state = "secure";
-			});
-		} else {
-			neighbours.forEach((p) => {
-				//if the cell is unexplored
-				if (
-					!this.#internalBoard[p.x][p.y].explored &&
-					this.#internalBoard[p.x][p.y].state != "secure"
-				)
-					this.#internalBoard[p.x][p.y].state = "insecure";
-			});
-		}
+    //I check that it isn't the WELL or the WUMPUS
+    if (this.#board[x][y].states.WELL || this.#board[x][y].states.WUMPUS) {
+      console.log("Game over");
+      return null;
+    }
 
-		//ahora ordenamos los vecinos siguiendo la heuristica
-		neighbours = this.#orderNeighbours(neighbours, this.#internalBoard);
-		//y los vamos visitando haciendo llamadas recursivas.
-		neighbours.forEach((p) => {
-			let res = this.explore(p);
-			if (res != null) {
-				return [currentPos, ...res];
-			}
-		});
-	}
+    //if it aint either of them => safe cell
+    this.#internalBoard[x][y].state = "safe";
+
+    //if not i get the neighbours
+    let neighbours = this.#getNeighbours({ x, y }, this.#size);
+
+    //If stench or breeze i set surrounding unexplored cells to dangerous
+    //If normal cell => surrounding are also safe
+    this.#internalBoard = this.#updateInternalState(
+      currentPos,
+      this.#internalBoard
+    );
+
+    console.log("Iteration: " + currentDepth + "--------------------");
+    console.log(currentPos);
+    console.log("total neighbours");
+    console.log(neighbours);
+
+    //now we order the neighbours and explore the optimal one
+    neighbours = this.#orderNeighbours(neighbours, this.#internalBoard);
+    if (neighbours.length == 0) {
+      console.log("No more paths");
+      return null;
+    }
+
+    //we define the situation
+    console.log("unexplored neighbours");
+    console.log(neighbours);
+    console.log("\n");
+
+    for (let i = 0; i < neighbours.length; i++) {
+      const res: Array<Position> = this.explore(
+        neighbours[i],
+        currentDepth + 1
+      );
+      if (res != null) {
+        const path = [currentPos, ...res];
+        console.log("path is");
+        console.log(path);
+        return path;
+      }
+    }
+
+    return null;
+  }
 }
--- react-wumpus/src/components/Board/Board.tsx
+++ react-wumpus/src/components/Board/Board.tsx
@@ -10,146 +10,148 @@ import { useInput } from "../../hooks/useInput";
 import useAiPlayer from "../../hooks/useAIPlayer";
 
 interface Props {
-	size: number;
-	className?: string;
+  size: number;
+  className?: string;
 }
 
 type GameState = "VICTORY" | "GAME OVER" | "PLAYING";
 
 const Board: React.FC<Props> = ({ size = 10, className = "" }) => {
-	const { board, visitCell, checkCell, resetBoard } = useBoard(size);
-	const [errorMsg, setErrorMsg] = useState<string>("");
-	const [modalVisible, setModalVisible] = useState<boolean>(false);
-	const { playerPos, updatePlayerPos } = useContext(PlayerPosContext);
-	const [hasGold, setHasGold] = useState(false);
-	const { gameState, setGameState } = useGameState("PLAYING", onStateChange);
-	const { playerInputEvent } = useInput();
-	const aiPlayer = useAiPlayer(size, board);
+  const { board, visitCell, checkCell, resetBoard } = useBoard(size);
+  const [errorMsg, setErrorMsg] = useState<string>("");
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const { playerPos, updatePlayerPos } = useContext(PlayerPosContext);
+  const [hasGold, setHasGold] = useState(false);
+  const { gameState, setGameState } = useGameState("PLAYING", onStateChange);
+  const { playerInputEvent } = useInput();
+  const aiPlayer = useAiPlayer(size, board);
 
-	const modalCallback = useCallback(() => {
-		if (gameState === "GAME OVER" || gameState === "VICTORY") {
-			gameCleanup();
-		}
-		setModalVisible(false);
-	}, [gameState]);
+  const modalCallback = useCallback(() => {
+    if (gameState === "GAME OVER" || gameState === "VICTORY") {
+      gameCleanup();
+    }
+    setModalVisible(false);
+  }, [gameState]);
 
-	useEffect(() => {
-		document.addEventListener(playerInputEvent.current.type, handleInput);
+  useEffect(() => {
+    document.addEventListener(playerInputEvent.current.type, handleInput);
 
-		return () => {
-			document.removeEventListener(playerInputEvent.current.type, handleInput);
-		};
-	}, [playerInputEvent, board, modalVisible]);
+    return () => {
+      document.removeEventListener(playerInputEvent.current.type, handleInput);
+    };
+  }, [playerInputEvent, board, modalVisible]);
 
-	useEffect(() => {
-		if (board.length != 0) {
-			aiPlayer.current.explore({ x: 0, y: 0 });
-		}
-	}, []);
+  useEffect(() => {
+    if (board.length != 0) {
+      const res = aiPlayer.current.explore({ x: 0, y: 0 });
+      console.log("The result is");
+      console.log(res);
+    }
+  }, [aiPlayer]);
 
-	function handleInput({ detail }: any) {
-		// function handleInput({ detail }: playerInputEvent) {
-		const direction = detail;
-		let tempPos: Position = { ...playerPos };
+  function handleInput({ detail }: any) {
+    // function handleInput({ detail }: playerInputEvent) {
+    const direction = detail;
+    const tempPos: Position = { ...playerPos };
 
-		if (modalVisible) return;
+    if (modalVisible) return;
 
-		switch (direction) {
-			case "UP":
-				tempPos.y--;
-				break;
+    switch (direction) {
+      case "UP":
+        tempPos.y--;
+        break;
 
-			case "DOWN":
-				tempPos.y++;
-				break;
+      case "DOWN":
+        tempPos.y++;
+        break;
 
-			case "LEFT":
-				tempPos.x--;
-				break;
+      case "LEFT":
+        tempPos.x--;
+        break;
 
-			case "RIGHT":
-				tempPos.x++;
-				break;
-		}
+      case "RIGHT":
+        tempPos.x++;
+        break;
+    }
 
-		if (!comparePosition(tempPos, playerPos)) movePlayer(tempPos);
-	}
+    if (!comparePosition(tempPos, playerPos)) movePlayer(tempPos);
+  }
 
-	function onStateChange(state: GameState) {
-		if (state === "GAME OVER") {
-			setModalVisible(true);
-			setErrorMsg("Game over");
-		} else if (state === "VICTORY") {
-			setModalVisible(true);
-			setErrorMsg("VICTORY !!!");
-		}
-	}
+  function onStateChange(state: GameState) {
+    if (state === "GAME OVER") {
+      setModalVisible(true);
+      setErrorMsg("Game over");
+    } else if (state === "VICTORY") {
+      setModalVisible(true);
+      setErrorMsg("VICTORY !!!");
+    }
+  }
 
-	function gameCleanup() {
-		resetBoard();
-		updatePlayerPos({ x: 1, y: 1 });
-		setHasGold(false);
-		setGameState("PLAYING");
-	}
+  function gameCleanup() {
+    resetBoard();
+    updatePlayerPos({ x: 1, y: 1 });
+    setHasGold(false);
+    setGameState("PLAYING");
+  }
 
-	//this could / should be a useEffect ?
-	function movePlayer({ x, y }: Position) {
-		//visit the previous cell (just in case)
-		let playerHasGold: boolean = hasGold;
-		visitCell(playerPos);
+  //this could / should be a useEffect ?
+  function movePlayer({ x, y }: Position) {
+    //visit the previous cell (just in case)
+    let playerHasGold: boolean = hasGold;
+    visitCell(playerPos);
 
-		let err = updatePlayerPos({ x, y });
-		if (err === -1) {
-			setModalVisible(true);
-			setErrorMsg("The player is out of bounds");
-			return;
-		}
+    const err = updatePlayerPos({ x, y });
+    if (err === -1) {
+      setModalVisible(true);
+      setErrorMsg("The player is out of bounds");
+      return;
+    }
 
-		//check wumpus / well
-		visitCell({ x, y });
-		if (
-			checkCell({ x, y }).states.WUMPUS === true ||
-			checkCell({ x, y }).states.WELL === true
-		) {
-			setGameState("GAME OVER");
-			return;
-		}
+    //check wumpus / well
+    visitCell({ x, y });
+    if (
+      checkCell({ x, y }).states.WUMPUS === true ||
+      checkCell({ x, y }).states.WELL === true
+    ) {
+      setGameState("GAME OVER");
+      return;
+    }
 
-		if (checkCell({ x, y }).states.GOLD) {
-			playerHasGold = true;
-			setHasGold(playerHasGold);
-		}
+    if (checkCell({ x, y }).states.GOLD) {
+      playerHasGold = true;
+      setHasGold(playerHasGold);
+    }
 
-		if (playerHasGold && checkCell({ x, y }).states.START) {
-			setGameState("VICTORY");
-		}
-	}
+    if (playerHasGold && checkCell({ x, y }).states.START) {
+      setGameState("VICTORY");
+    }
+  }
 
-	const dinamicBoardStyes = {
-		"--board-size": size,
-	} as React.CSSProperties;
+  const dinamicBoardStyes = {
+    "--board-size": size,
+  } as React.CSSProperties;
 
-	return (
-		<div className={`board ${className}`} style={dinamicBoardStyes}>
-			<Player position={playerPos} hasGold={hasGold}></Player>
-			{board.map((row) => {
-				return row.map(({ states, visited, position }: CellType) => {
-					return (
-						<Cell
-							states={states}
-							key={position.x + position.y}
-							position={position}
-							visited={visited}
-						></Cell>
-					);
-				});
-			})}
+  return (
+    <div className={`board ${className}`} style={dinamicBoardStyes}>
+      <Player position={playerPos} hasGold={hasGold}></Player>
+      {board.map((row) => {
+        return row.map(({ states, visited, position }: CellType) => {
+          return (
+            <Cell
+              states={states}
+              key={position.x + position.y}
+              position={position}
+              visited={visited}
+            ></Cell>
+          );
+        });
+      })}
 
-			<Modal visible={modalVisible} onModalClose={modalCallback}>
-				<h1>{errorMsg}</h1>
-			</Modal>
-		</div>
-	);
+      <Modal visible={modalVisible} onModalClose={modalCallback}>
+        <h1>{errorMsg}</h1>
+      </Modal>
+    </div>
+  );
 };
 
 export default Board;
