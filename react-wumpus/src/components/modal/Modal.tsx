16cf48be1944ad68151a5d9c77166c876d5bf6c3
guillermoVicenteGonzalez <guillermovg@usal.es>
2024-10-19 14:29:38 +0200

    modal misteriously working

--- react-wumpus/src/components/modal/Modal.tsx
+++ react-wumpus/src/components/modal/Modal.tsx
@@ -1,42 +1,45 @@
-import { useEffect } from "react";
-import { useInput } from "../../hooks/useInput";
+import { EventHandler, useEffect } from "react";
+import { playerInputEventType, useInput } from "../../hooks/useInput";
 
 interface Props {
-	visible: boolean;
-	children: React.ReactNode;
-	onModalClose: () => void;
+  visible: boolean;
+  children: React.ReactNode;
+  onModalClose: () => void;
 }
 
 const Modal: React.FC<Props> = ({ visible, children, onModalClose }) => {
-	const visibleStyles = visible ? "modal--visible" : "modal--hidden";
-	const { playerInputEvent } = useInput();
+  const visibleStyles = visible ? "modal--visible" : "modal--hidden";
+  const { playerInputEvent } = useInput();
 
-	useEffect(() => {
-		document.addEventListener(playerInputEvent.current.type, handleCancelKey);
+  useEffect(() => {
+    document.addEventListener(
+      playerInputEvent.current.type,
+      handleCancelKey as EventListener
+    );
 
-		return () => {
-			document.removeEventListener(
-				playerInputEvent.current.type,
-				handleCancelKey
-			);
-		};
-	}, [onModalClose]);
+    return () => {
+      document.removeEventListener(
+        playerInputEvent.current.type,
+        handleCancelKey as EventListener
+      );
+    };
+  }, [onModalClose]);
 
-	function handleCancelKey({ detail }: any) {
-		// function handleCancelKey({ detail }: playerInputEvent) {
-		if (detail == "CANCEL" || detail == "ACCEPT") {
-			onModalClose();
-		}
-	}
+  function handleCancelKey({ detail }: playerInputEventType) {
+    // function handleCancelKey({ detail }: playerInputEvent) {
+    if (detail == "CANCEL" || detail == "ACCEPT") {
+      onModalClose();
+    }
+  }
 
-	return (
-		<div className={`modal ${visibleStyles}`}>
-			<div className="modal__content">{children}</div>
-			<div className="modal__controls">
-				<button onClick={onModalClose}>Accept</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className={`modal ${visibleStyles}`}>
+      <div className="modal__content">{children}</div>
+      <div className="modal__controls">
+        <button onClick={onModalClose}>Accept</button>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
